39d455f648d1c7dbd6bcfac4472a4da8270a2474
Administrator <Administrator@DESKTOP-RMA9FF4>
2023-05-15 18:22:59 +0900

    5월 15일 수업

--- J0512_01/src/Class0512_09.java
+++ J0512_01/src/Class0512_09.java
@@ -1,15 +1,21 @@
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Class0512_09 {
 
 	public static void main(String[] args) {
-		//Arrays.sort(arr);   //오름차순 정렬
-				//Arrays.sort(arr,Collections.reverseOrder());  //내림차순정렬
-				int num = 10;
-				//단순 for문
-				for(int i:arr) {
-					System.out.println(i+" ");
-				}
+		Integer[] arr = {1,11,23,45,2,3,9,5};
+		//Arrays.sort(arr);  //오름차순정렬
+		//Arrays.sort(arr,Collections.reverseOrder()); //내림차순정렬
+		int num=10;
+		//단순for문
+		for(int i:arr) {
+			System.out.print(i+" ");
+		}
 
+//		for(int i=0;i<arr.length;i++) {
+//			System.out.print(arr[i]+" ");
+//		}
 	}
 
 }
